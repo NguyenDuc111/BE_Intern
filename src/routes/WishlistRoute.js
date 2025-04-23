630a4bb9347e75712873013dd3b1cb6afcd590dd
Haaju14 <phuchau22923@gmail.com>
2025-04-23 11:00:51 +0700

    be

--- src/routes/WishlistRoute.js
+++ src/routes/WishlistRoute.js
@@ -8,8 +8,11 @@ import { isAuthenticated } from "../middleware/auth.js";
 
 const router = express.Router();
 
+//endpoint thêm yêu thích
 router.post("/wishlist-add", isAuthenticated, addToWishlist);
+//endpoint lấy danh sách sản phẩm yêu thích
 router.get("/wishlist-all", isAuthenticated, getWishlist);
+//endpoint xóa yêu thích
 router.delete("/wishlist-delete/:id", isAuthenticated, removeFromWishlist);
 
 export default router;
