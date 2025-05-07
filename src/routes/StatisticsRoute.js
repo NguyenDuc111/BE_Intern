d95dc45d9fa0a0456d96524e0e66e2d017e9747e
Haaju14 <phuchau22923@gmail.com>
2025-05-07 16:17:50 +0700

    be

--- src/routes/StatisticsRoute.js
+++ src/routes/StatisticsRoute.js
@@ -1,10 +1,10 @@
-// import express from "express";
-// import { getRevenueStatistics } from "../controllers/StatisticsController.js";
-// import { isAuthenticated, isAdmin } from "../middleware/auth.js";
+import express from "express";
+import { getRevenueStatistics } from "../controllers/StatisticsController.js";
+import { isAuthenticated, isAdmin } from "../middleware/auth.js";
 
-// const router = express.Router();
+const router = express.Router();
 
-// // Thống kê doanh thu
-// router.get("/static", isAuthenticated, isAdmin, getRevenueStatistics);
+// Thống kê doanh thu
+router.get("/static", isAuthenticated, isAdmin, getRevenueStatistics);
 
-// export default router;
+export default router;
