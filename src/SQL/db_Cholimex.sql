bf9fccd019f95a29b8eca05e0325072ff3134d91
Haaju14 <phuchau22923@gmail.com>
2025-05-07 09:42:40 +0700

    be

--- src/SQL/db_Cholimex.sql
+++ src/SQL/db_Cholimex.sql
@@ -20,7 +20,7 @@ CREATE TABLE `Cart` (
   KEY `ProductID` (`ProductID`),
   CONSTRAINT `Cart_ibfk_1` FOREIGN KEY (`UserID`) REFERENCES `Users` (`UserID`) ON DELETE CASCADE,
   CONSTRAINT `Cart_ibfk_2` FOREIGN KEY (`ProductID`) REFERENCES `Products` (`ProductID`) ON DELETE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=41 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=43 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 DROP TABLE IF EXISTS `Categories`;
 CREATE TABLE `Categories` (
@@ -44,7 +44,7 @@ CREATE TABLE `LoyaltyPoints` (
   PRIMARY KEY (`PointID`),
   KEY `UserID` (`UserID`),
   CONSTRAINT `LoyaltyPoints_ibfk_1` FOREIGN KEY (`UserID`) REFERENCES `Users` (`UserID`) ON DELETE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=38 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=40 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 DROP TABLE IF EXISTS `Notification`;
 CREATE TABLE `Notification` (
@@ -74,7 +74,7 @@ CREATE TABLE `OrderDetails` (
   KEY `ProductID` (`ProductID`),
   CONSTRAINT `OrderDetails_ibfk_1` FOREIGN KEY (`OrderID`) REFERENCES `Orders` (`OrderID`) ON DELETE CASCADE,
   CONSTRAINT `OrderDetails_ibfk_2` FOREIGN KEY (`ProductID`) REFERENCES `Products` (`ProductID`) ON DELETE SET NULL
-) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 DROP TABLE IF EXISTS `Orders`;
 CREATE TABLE `Orders` (
@@ -86,12 +86,15 @@ CREATE TABLE `Orders` (
   `ShippingAddress` text,
   `createdAt` datetime DEFAULT NULL,
   `updatedAt` datetime DEFAULT NULL,
+  `VoucherCode` varchar(6) DEFAULT NULL,
   PRIMARY KEY (`OrderID`),
   KEY `UserID` (`UserID`),
   KEY `PromotionID` (`PromotionID`),
+  KEY `Orders_ibfk_3` (`VoucherCode`),
   CONSTRAINT `Orders_ibfk_1` FOREIGN KEY (`UserID`) REFERENCES `Users` (`UserID`) ON DELETE SET NULL,
-  CONSTRAINT `Orders_ibfk_2` FOREIGN KEY (`PromotionID`) REFERENCES `Promotion` (`PromotionID`) ON DELETE SET NULL
-) ENGINE=InnoDB AUTO_INCREMENT=74 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  CONSTRAINT `Orders_ibfk_2` FOREIGN KEY (`PromotionID`) REFERENCES `Promotion` (`PromotionID`) ON DELETE SET NULL,
+  CONSTRAINT `Orders_ibfk_3` FOREIGN KEY (`VoucherCode`) REFERENCES `UserVouchers` (`Code`) ON DELETE SET NULL
+) ENGINE=InnoDB AUTO_INCREMENT=75 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 DROP TABLE IF EXISTS `ProductCategories`;
 CREATE TABLE `ProductCategories` (
@@ -205,7 +208,7 @@ CREATE TABLE `TempOrderItems` (
   KEY `ProductID` (`ProductID`),
   CONSTRAINT `TempOrderItems_ibfk_1` FOREIGN KEY (`OrderID`) REFERENCES `Orders` (`OrderID`),
   CONSTRAINT `TempOrderItems_ibfk_2` FOREIGN KEY (`ProductID`) REFERENCES `Products` (`ProductID`)
-) ENGINE=InnoDB AUTO_INCREMENT=50 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=51 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 DROP TABLE IF EXISTS `Users`;
 CREATE TABLE `Users` (
@@ -239,7 +242,7 @@ CREATE TABLE `UserVouchers` (
   KEY `VoucherID` (`VoucherID`),
   CONSTRAINT `UserVouchers_ibfk_1` FOREIGN KEY (`UserID`) REFERENCES `Users` (`UserID`) ON DELETE CASCADE,
   CONSTRAINT `UserVouchers_ibfk_2` FOREIGN KEY (`VoucherID`) REFERENCES `Vouchers` (`VoucherID`) ON DELETE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 DROP TABLE IF EXISTS `Vouchers`;
 CREATE TABLE `Vouchers` (
@@ -249,6 +252,7 @@ CREATE TABLE `Vouchers` (
   `PointsRequired` int NOT NULL,
   `UsageLimit` int NOT NULL,
   `ExpiryDays` int NOT NULL,
+  `RedemptionLimit` int NOT NULL DEFAULT '1',
   PRIMARY KEY (`VoucherID`)
 ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
@@ -269,7 +273,8 @@ CREATE TABLE `Wishlists` (
 INSERT INTO `Cart` (`CartID`, `UserID`, `ProductID`, `Quantity`, `createdAt`, `updatedAt`) VALUES
 (4, 6, 10, 10, '2025-04-25 07:48:53', '2025-04-25 08:46:23'),
 (5, 6, 6, 10, '2025-04-25 09:21:33', '2025-04-25 09:21:33'),
-(38, 5, 4, 1, '2025-05-03 03:29:46', '2025-05-06 02:05:20');
+(38, 5, 4, 1, '2025-05-03 03:29:46', '2025-05-06 02:05:20'),
+(42, 9, 5, 1, '2025-05-07 02:26:28', '2025-05-07 02:26:28');
 INSERT INTO `Categories` (`CategoryID`, `CategoryName`, `Description`, `ImageURL`, `createdAt`, `updatedAt`) VALUES
 (1, 'Nước mắm', 'Các loại nước mắm cao cấp từ Cholimex', '/images/nuoc-mam.jpg', '2025-04-23 06:31:25', '2025-04-23 06:31:25'),
 (2, 'Tương ớt', 'Tương ớt cay nồng, chất lượng', '/images/tuong-ot.jpg', '2025-04-23 06:31:25', '2025-04-23 06:31:25'),
@@ -287,7 +292,9 @@ INSERT INTO `LoyaltyPoints` (`PointID`, `UserID`, `Points`, `Description`, `crea
 (34, 5, -8, 'Dùng 8 điểm để giảm giá đơn hàng', '2025-05-03 01:09:50', '2025-05-03 01:09:50'),
 (35, 9, 75, 'Điểm từ đơn hàng #2', '2025-04-23 06:31:25', '2025-04-23 06:31:25'),
 (36, 9, -5, 'Đổi voucher Giảm 10,000 VND', '2025-05-07 01:51:30', '2025-05-07 01:51:30'),
-(37, 9, -3, 'Dùng 3 điểm để giảm giá đơn hàng', '2025-05-07 01:59:05', '2025-05-07 01:59:05');
+(37, 9, -3, 'Dùng 3 điểm để giảm giá đơn hàng', '2025-05-07 01:59:05', '2025-05-07 01:59:05'),
+(38, 9, -15, 'Đổi voucher Giảm 10%', '2025-05-07 02:21:39', '2025-05-07 02:21:39'),
+(39, 9, -2, 'Dùng 2 điểm để giảm giá đơn hàng', '2025-05-07 02:21:58', '2025-05-07 02:21:58');
 INSERT INTO `Notification` (`NotificationID`, `UserID`, `Title`, `Message`, `IsRead`, `createdAt`, `updatedAt`) VALUES
 (1, NULL, 'Sản phẩm mới tại Cholimex!', 'Sản phẩm mới: Nước mắm Cholimex 750ml đã có mặt tại Cholimex!', 1, '2025-04-23 06:31:25', '2025-05-03 04:46:00'),
 (2, 2, 'Sản phẩm mới tại Cholimex!', 'Sản phẩm mới: Tương ớt Cholimex 750g đã có mặt tại Cholimex!', 0, '2025-04-23 06:31:25', '2025-04-23 07:33:10'),
@@ -300,26 +307,28 @@ INSERT INTO `OrderDetails` (`OrderDetailID`, `OrderID`, `ProductID`, `Quantity`,
 (17, 52, 3, 1, '15000.00', '2025-04-29 07:47:33', '2025-04-29 07:47:33'),
 (18, 71, 3, 16, '15000.00', '2025-05-03 01:10:21', '2025-05-03 01:10:21'),
 (19, 71, 8, 1, '30000.00', '2025-05-03 01:10:22', '2025-05-03 01:10:22'),
-(20, 73, 3, 2, '15000.00', '2025-05-07 01:59:35', '2025-05-07 01:59:35');
-INSERT INTO `Orders` (`OrderID`, `UserID`, `PromotionID`, `TotalAmount`, `Status`, `ShippingAddress`, `createdAt`, `updatedAt`) VALUES
-(20, 5, 1, '2320000.00', 'Paid', 'HCM', '2025-04-29 02:05:12', '2025-04-29 02:06:59'),
-(21, 5, NULL, '260000.00', 'Cancelled', 'acx', '2025-04-29 02:28:30', '2025-04-29 02:36:19'),
-(47, 5, NULL, '1113000.00', 'Processing', 'asxzxczcxczxc', NULL, NULL),
-(48, 5, NULL, '1095000.00', 'Cancelled', 'asdzxc', NULL, NULL),
-(49, 5, NULL, '210000.00', 'Cancelled', 'zxczxc', NULL, NULL),
-(50, 5, NULL, '60000.00', 'Cancelled', 'vvvv', NULL, NULL),
-(52, 5, NULL, '15000.00', 'Paid', 'il', NULL, NULL),
-(54, 5, 1, '54000.00', 'Processing', '123', NULL, NULL),
-(55, 5, NULL, '88000.00', 'Cancelled', 'asdasdasd', NULL, NULL),
-(56, 5, NULL, '15000.00', 'Pending', 'axzccxzzcxzxczxc', NULL, NULL),
-(57, 5, NULL, '15000.00', 'Pending', 'd', NULL, NULL),
-(59, 5, NULL, '28000.00', 'Pending', 'olololl', NULL, NULL),
-(60, 5, NULL, '15000.00', 'Pending', 'asdsasdsad', NULL, NULL),
-(61, 5, NULL, '45000.00', 'Pending', 'asdsadsad', NULL, NULL),
-(70, 5, NULL, '45000.00', 'Pending', 'zxc', NULL, NULL),
-(71, 9, NULL, '262000.00', 'Paid', 'zxc', NULL, NULL),
-(72, 9, NULL, '45000.00', 'Paid', '123', NULL, NULL),
-(73, 9, NULL, '27000.00', 'Paid', 'asd', NULL, NULL);
+(20, 73, 3, 2, '15000.00', '2025-05-07 01:59:35', '2025-05-07 01:59:35'),
+(21, 74, 8, 2, '30000.00', '2025-05-07 02:23:13', '2025-05-07 02:23:13');
+INSERT INTO `Orders` (`OrderID`, `UserID`, `PromotionID`, `TotalAmount`, `Status`, `ShippingAddress`, `createdAt`, `updatedAt`, `VoucherCode`) VALUES
+(20, 5, 1, '2320000.00', 'Paid', 'HCM', '2025-04-29 02:05:12', '2025-04-29 02:06:59', NULL),
+(21, 5, NULL, '260000.00', 'Cancelled', 'acx', '2025-04-29 02:28:30', '2025-04-29 02:36:19', NULL),
+(47, 5, NULL, '1113000.00', 'Processing', 'asxzxczcxczxc', NULL, NULL, NULL),
+(48, 5, NULL, '1095000.00', 'Cancelled', 'asdzxc', NULL, NULL, NULL),
+(49, 5, NULL, '210000.00', 'Cancelled', 'zxczxc', NULL, NULL, NULL),
+(50, 5, NULL, '60000.00', 'Cancelled', 'vvvv', NULL, NULL, NULL),
+(52, 5, NULL, '15000.00', 'Paid', 'il', NULL, NULL, NULL),
+(54, 5, 1, '54000.00', 'Processing', '123', NULL, NULL, NULL),
+(55, 5, NULL, '88000.00', 'Cancelled', 'asdasdasd', NULL, NULL, NULL),
+(56, 5, NULL, '15000.00', 'Pending', 'axzccxzzcxzxczxc', NULL, NULL, NULL),
+(57, 5, NULL, '15000.00', 'Pending', 'd', NULL, NULL, NULL),
+(59, 5, NULL, '28000.00', 'Pending', 'olololl', NULL, NULL, NULL),
+(60, 5, NULL, '15000.00', 'Pending', 'asdsasdsad', NULL, NULL, NULL),
+(61, 5, NULL, '45000.00', 'Pending', 'asdsadsad', NULL, NULL, NULL),
+(70, 5, NULL, '45000.00', 'Pending', 'zxc', NULL, NULL, NULL),
+(71, 9, NULL, '262000.00', 'Paid', 'zxc', NULL, NULL, NULL),
+(72, 9, NULL, '45000.00', 'Paid', '123', NULL, NULL, NULL),
+(73, 9, NULL, '27000.00', 'Paid', 'asd', NULL, NULL, NULL),
+(74, 9, NULL, '52000.00', 'Paid', 'asd', NULL, NULL, NULL);
 INSERT INTO `ProductCategories` (`ProductID`, `CategoryID`, `createdAt`, `updatedAt`) VALUES
 (1, 1, '2025-04-25 06:54:46', '2025-04-25 06:54:46'),
 (2, 1, '2025-04-25 06:54:46', '2025-04-25 06:54:46'),
@@ -344,7 +353,7 @@ INSERT INTO `Products` (`ProductID`, `ProductName`, `Description`, `Price`, `Sto
 (5, 'Gia vị nêm Cholimex 100g', 'Gia vị nêm đa dụng, giúp món ăn thêm đậm đà, dễ sử dụng cho các món canh, xào.', '10000.00', 148, NULL, '2025-04-05 09:00:00', '2025-04-29 07:43:54', 'Muối, đường, bột ngọt'),
 (6, 'Tương ớt Cholimex 500g', 'Tương ớt cay đậm, đóng gói lớn 500g, phù hợp cho nhà hàng hoặc gia đình đông người.', '30000.00', 128, NULL, '2025-04-06 11:00:00', '2025-04-29 01:12:54', 'Ớt, tỏi, đường, muối'),
 (7, 'Nước mắm Cholimex Cao cấp 750ml', 'Nước mắm cao cấp, độ đạm cao, hương vị tinh tế, dùng cho các món ăn cao cấp.', '60000.00', 60, NULL, '2025-04-07 13:00:00', '2025-04-25 06:56:21', 'Cá cơm, muối, nước, đường'),
-(8, 'Nước tương Cholimex 500ml', 'Nước tương tự nhiên, đóng chai 500ml, phù hợp để ướp thịt, cá hoặc làm nước chấm.', '30000.00', 55, 'https://cholimexfood.com.vn/wp-content/uploads/2019/12/TUONG-OT-250G-500x500.png', '2025-04-08 15:00:00', '2025-05-03 03:28:01', 'Đậu nành, muối, đường'),
+(8, 'Nước tương Cholimex 500ml', 'Nước tương tự nhiên, đóng chai 500ml, phù hợp để ướp thịt, cá hoặc làm nước chấm.', '30000.00', 53, 'https://cholimexfood.com.vn/wp-content/uploads/2019/12/TUONG-OT-250G-500x500.png', '2025-04-08 15:00:00', '2025-05-07 02:23:13', 'Đậu nành, muối, đường'),
 (9, 'Gia vị nêm Cholimex 200g', 'Gia vị nêm tiện lợi, đóng gói 200g, giúp món ăn thêm ngon, phù hợp mọi bữa ăn.', '18000.00', 179, NULL, '2025-04-09 17:00:00', '2025-04-29 07:35:04', 'Muối, đường, bột ngọt'),
 (10, 'Tương cà Cholimex 300g', 'Tương cà thơm ngon, làm từ cà chua tươi, dùng để chấm hoặc nấu ăn.', '20000.00', 105, NULL, '2025-04-10 10:00:00', '2025-04-25 08:46:23', 'Cà chua, đường, muối, giấm'),
 (11, 'Dầu hào Cholimex 250ml', 'Dầu hào đậm đà, chiết xuất từ hàu, dùng để ướp hoặc xào các món rau, thịt.', '25000.00', 69, NULL, '2025-04-11 12:00:00', '2025-04-29 07:35:04', 'Hàu, muối, đường, bột ngọt'),
@@ -398,13 +407,14 @@ INSERT INTO `Users` (`UserID`, `RoleID`, `FullName`, `Email`, `Password`, `Phone
 (8, 2, 'duccccc', 'duckute395@gmail.com', '$2b$10$M0vNXzG.AboxeZrQ9b5C9u4wOzeOitgGVSa8QMbs6n/mkILRo4zyG', '123456789', 'asdasd', '2025-05-06 02:09:23', '2025-05-06 02:12:44'),
 (9, 2, 'Nguyen Phuc Hau', 'haajuuu8@gmail.com', '$2b$10$/wvxp1InY9AWZEj/2eqbuuKsVnJa9PR4p96CrnSHFSq5Y1Z82bBb6', '123456789', 'Khu Công nghệ cao Xa lộ Hà Nội, Hiệp Phú, Thủ Đức,', '2025-05-06 02:16:43', '2025-05-07 01:12:45');
 INSERT INTO `UserVouchers` (`UserVoucherID`, `UserID`, `VoucherID`, `Code`, `Status`, `UsageCount`, `ExpiryDate`) VALUES
-(1, 9, 1, 'V7ODP5', 'used', 2, '2025-06-06 01:51:30');
-INSERT INTO `Vouchers` (`VoucherID`, `Name`, `DiscountValue`, `PointsRequired`, `UsageLimit`, `ExpiryDays`) VALUES
-(1, 'Giảm 10,000 VND', '10000.00', 5, 2, 30),
-(2, 'Giảm 20,000 VND', '20000.00', 10, 2, 30),
-(3, 'Giảm 50,000 VND', '50000.00', 20, 1, 30),
-(4, 'Giảm 100,000 VND', '100000.00', 40, 1, 30),
-(5, 'Giảm 10%', '10.00', 15, 3, 30);
+(1, 9, 1, 'V7ODP5', 'used', 2, '2025-06-06 01:51:30'),
+(2, 9, 5, '6F0ENZ', 'active', 0, '2025-06-06 02:21:39');
+INSERT INTO `Vouchers` (`VoucherID`, `Name`, `DiscountValue`, `PointsRequired`, `UsageLimit`, `ExpiryDays`, `RedemptionLimit`) VALUES
+(1, 'Giảm 10,000 VND', '10000.00', 5, 2, 30, 1),
+(2, 'Giảm 20,000 VND', '20000.00', 10, 2, 30, 1),
+(3, 'Giảm 50,000 VND', '50000.00', 20, 1, 30, 1),
+(4, 'Giảm 100,000 VND', '100000.00', 40, 1, 30, 1),
+(5, 'Giảm 10%', '10.00', 15, 3, 30, 1);
 INSERT INTO `Wishlists` (`WishlistID`, `UserID`, `ProductID`, `createdAt`, `updatedAt`) VALUES
 (4, 5, 2, '2025-04-29 02:56:41', '2025-04-29 02:56:41'),
 (5, 5, 3, '2025-04-29 03:27:32', '2025-04-29 03:27:32'),
